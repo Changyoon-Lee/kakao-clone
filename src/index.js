fa465ff270c818d622f109f83afb1932010371a8
Changyoon-Lee <lckddbsl@gmail.com>
2023-01-15 23:39:20 +0900

    friend 페이지 pug 형식 적용, webpack 적용및 css 설정 예정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 
 import express from "express";
 import morgan from "morgan"
-// import globalRouter from "./router";
+import globalRouter from "./router";
 
 const app = express();
 const logger = morgan("dev");
 const port = process.env.PORT || 3000;
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 
-// app.use("/", globalRouter);
-app.use(express.static(process.cwd() + "/public"));
+app.use("/", globalRouter);
+// app.use(express.static(process.cwd() + "/public"));
 
 app.listen(port, () => console.log("running servere on localhost:3000"));
